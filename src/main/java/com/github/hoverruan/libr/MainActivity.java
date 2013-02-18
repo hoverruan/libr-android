5d05a3926bb2a1b8b6faa1367b7f06d8fe6bb426
Hover Ruan <hoverruan@gmail.com>
2013-02-18 16:52:56 +0800

    Create android maven project

---
+++ src/main/java/com/github/hoverruan/libr/MainActivity.java
@@ -0,0 +1,16 @@
+package com.github.hoverruan.libr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.github.hoverruan.R;
+
+/**
+ * @author Hover Ruan
+ */
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+}
